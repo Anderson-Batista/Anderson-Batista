da1daab6b0b1529dc0a823ca25fbd7b572a74b73
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-06-24 09:26:50 -0300

    Inserindo mais uma linha

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World GIT");
 		System.out.println("Prática 02 GIT");
 		System.out.println("Alteração no codigo para commit");
+		System.out.println("Inserindo mais uma linha para commitar");
 	}
 
 }
