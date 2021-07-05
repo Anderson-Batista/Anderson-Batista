4867d5e4330a1a77e1b6d51fff8a2cd5eaed0f6e
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-07-05 08:03:43 -0300

    realizando commit na branch master

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Alteração no codigo para commit");
 		System.out.println("Inserindo mais uma linha para commitar");
 		System.out.println("Parte 3 Passo 4 da pratica 02");
+		System.out.println("commitando na branch master");
 	}
 
 }
